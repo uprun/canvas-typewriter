56ccc03357a1b4fff2c53d2918d2868ae585ba88
Oleksandr Kryvonos <geraclion@gmail.com>
2023-04-13 12:45:51 +0200

    generating bundles properly

--- Controllers/BundleController.cs
+++ Controllers/BundleController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -19,18 +20,60 @@ namespace lisperanto.Controllers
             _logger = logger;
         }
 
-        public async Task GenerateBundle()
+        public async Task GenerateBundle(string path)
         {
-            Console.WriteLine(nameof(GenerateBundle));
-            string input_path = Path.Combine(Directory.GetCurrentDirectory(), "Views", "Home", "ideas.cshtml");
-            string output_path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "bundle_ideas.html");
-            await helper_generate_bundle(input_path, output_path);
+            Console.WriteLine($"{nameof(GenerateBundle)} for \"{path}\"");
+            string input_path = Path.Combine(Directory.GetCurrentDirectory(), path);
+            var without_extension = Path.GetFileNameWithoutExtension(input_path);
+            var memory_stream = await helper_generate_bundle(input_path, add_script_tags: true);
+            var hash = await generate_hash(memory_stream);
+            var output_name = $"{without_extension}--{hash}{Path.GetExtension(input_path)}";
+            string output_path = Path.Combine(Directory.GetCurrentDirectory(), "bundles", output_name);
+            var directory = Path.GetDirectoryName(output_path);
+            if (Directory.Exists(directory) == false)
+            {
+                Directory.CreateDirectory(directory);
+            }
+            using(StreamWriter actual_writer = new StreamWriter(output_path))
+            {
+                memory_stream.WriteTo(actual_writer.BaseStream);
+                actual_writer.Flush();
+            }
+            Response.ContentType = "text/html";
+            var result = new MemoryStream();
+            using(StreamWriter html_stream_writer = new StreamWriter(result))
+            {
+                string path2 = Path.Combine("bundles", output_name);
+                string some = Request.IsHttps ? "https://" : "http://";
+                html_stream_writer.WriteLine($"<a href=\"{some}{Request.Host}/{path2}\" target=\"blank\" >{hash}</a>");
+                html_stream_writer.Flush();
+                result.Seek(0, SeekOrigin.Begin);
+                await result.CopyToAsync(Response.Body);
+            }
+
+
         }
 
-        private static async Task helper_generate_bundle(string input_path, string output_path, bool add_script_tags = true)
+        public async Task<string> generate_hash(MemoryStream in_memory_stream)
         {
+            string string_hash = "";
+            in_memory_stream.Seek(0, SeekOrigin.Begin);
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                var hash = await sha256Hash.ComputeHashAsync(in_memory_stream);
+                string_hash = String.Join("", hash.Select(h => h.ToString("x2")));
+            }
+            in_memory_stream.Seek(0, SeekOrigin.Begin);
+            return string_hash;
+
+        }
+
+        private static async Task<MemoryStream> helper_generate_bundle(string input_path, bool add_script_tags = true)
+        {
+            var just_copy = new MemoryStream();
+            MemoryStream in_memory_stream = new MemoryStream();
             using (StreamReader input_source = new StreamReader(input_path))
-            using (StreamWriter output_writer = new StreamWriter(output_path))
+            using (StreamWriter output_writer = new StreamWriter(in_memory_stream))
             {
                 while (input_source.EndOfStream == false)
                 {
@@ -56,7 +99,11 @@ namespace lisperanto.Controllers
                     
 
                 }
+                
+                in_memory_stream.WriteTo(just_copy);
+                just_copy.Seek(0, SeekOrigin.Begin);
             }
+            return just_copy;
         }
 
         private static void embed_style_content(StreamWriter output_writer, string trimmed)

a074f0b233ce23ddfd090b16b2831befaf0102bf
Oleksandr Kryvonos <geraclion@gmail.com>
2023-05-29 09:44:17 +0200

    create a version-controller

--- Controllers/BundleController.cs
+++ Controllers/BundleController.cs
@@ -20,62 +20,8 @@ namespace lisperanto.Controllers
             subscribe_to_file_changes();
         }
 
-        private void subscribe_to_file_changes()
-        {
-            if (watcher != null)
-                return;
-
-            this.watcher = new FileSystemWatcher(Directory.GetCurrentDirectory());
-
-            watcher.NotifyFilter = NotifyFilters.Attributes
-                                 | NotifyFilters.CreationTime
-                                 | NotifyFilters.DirectoryName
-                                 | NotifyFilters.FileName
-                                 | NotifyFilters.LastAccess
-                                 | NotifyFilters.LastWrite
-                                 | NotifyFilters.Security
-                                 | NotifyFilters.Size;
-
-            watcher.Changed += OnChanged;
-            watcher.Created += OnCreated;
-            watcher.Deleted += OnDeleted;
-            watcher.Renamed += OnRenamed;
-            watcher.Error += OnError;
-
-            watcher.IncludeSubdirectories = true;
-            watcher.EnableRaisingEvents = true;
-        }
-
-        private void OnError(object sender, ErrorEventArgs e)
-        {
-            Console.WriteLine(e);
-        }
-
-        private static void OnChanged(object sender, FileSystemEventArgs e)
-        {
-            if (e.ChangeType != WatcherChangeTypes.Changed)
-            {
-                return;
-            }
-            Console.WriteLine($"Changed: {e.FullPath}");
-        }
-
-        private static void OnCreated(object sender, FileSystemEventArgs e)
-        {
-            string value = $"Created: {e.FullPath}";
-            Console.WriteLine(value);
-        }
-
-        private static void OnDeleted(object sender, FileSystemEventArgs e) =>
-            Console.WriteLine($"Deleted: {e.FullPath}");
-
-        private static void OnRenamed(object sender, RenamedEventArgs e)
-        {
-            Console.WriteLine($"Renamed:");
-            Console.WriteLine($"    Old: {e.OldFullPath}");
-            Console.WriteLine($"    New: {e.FullPath}");
-        }
-
+        
+//
         public async Task GenerateBundle(string path)
         {
             Console.WriteLine($"{nameof(GenerateBundle)} for \"{path}\"");
---
+++ version-controller/Program.cs
@@ -0,0 +1,64 @@
+﻿FileSystemWatcher subscribe_to_file_changes()
+{
+
+    var path = Path.Combine(Directory.GetCurrentDirectory(), "..");
+    var watcher = new FileSystemWatcher(path);
+    Console.WriteLine($"Watching \"{path}\"");
+
+    var history_path = Path.Combine(path, ".history");
+    System.IO.Directory.CreateDirectory(history_path);
+
+    watcher.NotifyFilter = NotifyFilters.Attributes
+                            | NotifyFilters.CreationTime
+                            | NotifyFilters.DirectoryName
+                            | NotifyFilters.FileName
+                            | NotifyFilters.LastAccess
+                            | NotifyFilters.LastWrite
+                            | NotifyFilters.Security
+                            | NotifyFilters.Size;
+
+    watcher.Changed += OnChanged;
+    watcher.Created += OnCreated;
+    watcher.Deleted += OnDeleted;
+    watcher.Renamed += OnRenamed;
+    watcher.Error += OnError;
+
+    watcher.IncludeSubdirectories = true;
+    watcher.EnableRaisingEvents = true;
+
+    return watcher;
+}
+
+void OnError(object sender, ErrorEventArgs e)
+{
+    Console.WriteLine(e);
+}
+
+void OnChanged(object sender, FileSystemEventArgs e)
+{
+    if (e.ChangeType != WatcherChangeTypes.Changed)
+    {
+        return;
+    }
+    Console.WriteLine($"Changed: {e.FullPath}");
+}
+
+void OnCreated(object sender, FileSystemEventArgs e)
+{
+    string value = $"Created: {e.FullPath}";
+    Console.WriteLine(value);
+}
+
+void OnDeleted(object sender, FileSystemEventArgs e) =>
+    Console.WriteLine($"Deleted: {e.FullPath}");
+
+void OnRenamed(object sender, RenamedEventArgs e)
+{
+    Console.WriteLine($"Renamed:");
+    Console.WriteLine($"    Old: {e.OldFullPath}");
+    Console.WriteLine($"    New: {e.FullPath}");
+}
+
+subscribe_to_file_changes();
+
+while(true){} // yeah infinite loop
